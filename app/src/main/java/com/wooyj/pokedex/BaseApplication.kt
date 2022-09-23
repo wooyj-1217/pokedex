49532b74ab1f1d5151fa5a00cc4f10ff1701b204
wooyj-1217 <shamoo1217@gmail.com>
2022-09-23 19:00:22 +0900

    base application file 정의

---
+++ app/src/main/java/com/wooyj/pokedex/BaseApplication.kt
@@ -0,0 +1,14 @@
+package com.wooyj.pokedex
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
